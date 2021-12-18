04be6e5719065190dc6659ea3c1922014fe71e69
twofish0808 <t108820024@ntut.edu.tw>
2021-12-18 17:56:03 +0800

    second

--- tha/perceptual.py
+++ tha/perceptual.py
@@ -1,7 +1,11 @@
+from torch.autograd.variable import Variable
 from torch.functional import Tensor
 from torchvision.models import vgg16,vgg16_bn
+from cnn_finetune import make_model
+import torch
 from torch.nn import MSELoss
 from torch import Tensor
+import numpy as np
 class perceptual_loss:
     def __init__(self,change,target,label) :
         self.loss=0
@@ -9,8 +13,12 @@ class perceptual_loss:
         self.label=label
         self.target=target
     def forword(self):
-        vg=vgg16(pretrained=True)
-        cov_change=vg(self.change,self.label)
-        cov_target=vg(self.target,self.label)
+        model=vgg16(pretrained=True)
+
+        print(self.change[0])
+        ch=self.change
+        tar=self.change
+        cov_change=model(ch)
+        cov_target=model(tar)
         self.loss=MSELoss(cov_change,cov_target)
         return self.loss
